be335b390f9949e9e617b3831a9ac5ce13a795bb
Mike Lang <mlang@student.neumont.edu>
2016-09-26 11:39:13 -0600

    Initial Commit - Small amount of work already done
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ NimState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim
+{
+    class NimState
+    {
+        public double averageState { get; private set; }
+        public int occuranceCount { get; private set; }
+
+        public void AppendAverage(double value)
+        {
+            averageState = ((averageState * occuranceCount) + value) / (occuranceCount + 1);
+            occuranceCount++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim
+{
+    class Program
+    {
+        private NimState[,,] states = new NimState[3, 5, 7];
+
+        private int[,,] pieces = new int[3, 5, 7];
+        static void Main(string[] args)
+        {
+        }
+    }
+}
