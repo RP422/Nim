882ae14bee3ae4839a952f7f1affd404ba1854be
Mike Lang <mlang@student.neumont.edu>
2016-09-26 11:59:12 -0600

    -Added a List for move history -Added helper comments
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace Nim
 {
     public class Program
     {
-        private NimState[,,] states = new NimState[3, 5, 7];
-
+        private NimState[,,] states = new NimState[4, 6, 8];
         private int[] pieces = new int[3];
+
+        private List<int[]> currentMoveHistory = new List<int[]>();
 
         static void Main(string[] args)
         {
@@ -20,11 +21,11 @@ namespace Nim
         private void InitializeNimStates()
         {
             // Builds every NimState object for learning purposes throughout runtime
-            for(int x = 0; x < 3; x++)
+            for(int x = 0; x < 4; x++)
             {
-                for (int y = 0; y < 5; y++)
+                for (int y = 0; y < 6; y++)
                 {
-                    for (int z = 0; z < 7; z++)
+                    for (int z = 0; z < 8; z++)
                     {
                         states[x, y, x] = new NimState();
                     }
