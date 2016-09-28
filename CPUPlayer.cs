c35224a1eae5d8ee3b679598c6f7599ca995967c
mandalore <centerback_rules@yahoo.com>
2016-09-28 11:09:08 -0600

    changed name to make more sense

--- CPUPlayer.cs
+++ CPUPlayer.cs
@@ -24,8 +24,8 @@ namespace Nim
             bool validInput;
             int row, amount = 0;
             Random r = new Random();
-            int[] hi = bestmoves[r.Next(bestmoves.Count)];
-            Console.WriteLine(hi[0] + "" + hi[1] + hi[2]);
+            int[] moveState = bestmoves[r.Next(bestmoves.Count)];
+            Console.WriteLine(moveState[0] + "" + moveState[1] + moveState[2]);
             //do
             //{
             //    row = r.Next(1, Game.pieces.Length + 1);
@@ -42,19 +42,19 @@ namespace Nim
             //    validInput = amount <= Game.pieces[row - 1] && amount > 0;
             //} while (!validInput);
             //Console.WriteLine("Computer is removing {0} pieces from row {1}", amount, row);
-            if (hi[0] != Game.pieces[0])
+            if (moveState[0] != Game.pieces[0])
             {
-                amount = Game.pieces[0] - hi[0];
+                amount = Game.pieces[0] - moveState[0];
                 row = 1;
             }
-            else if (hi[1] != Game.pieces[1])
+            else if (moveState[1] != Game.pieces[1])
             {
-                amount = Game.pieces[1] - hi[1];
+                amount = Game.pieces[1] - moveState[1];
                 row = 2;
             }
             else
             {
-                amount = Game.pieces[2] - hi[2];
+                amount = Game.pieces[2] - moveState[2];
                 row = 3;
             }
             move += row;
