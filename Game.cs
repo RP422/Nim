cbda8c431f1784f107024b6f99186391a1a3bd87
mandalore <centerback_rules@yahoo.com>
2016-10-03 11:34:10 -0600

    Added GamReviewer have not implemented yet so game review is still in comments for now

--- Game.cs
+++ Game.cs
@@ -149,57 +149,44 @@ namespace Nim
             return (pieces[0] == 0 && pieces[1] == 0 && pieces[2] == 0);
         }
 
-        public void ReviewGame()
-        {
-            int stateCountWin = currentMoveHistory.Count;
-            int stateCountLose = stateCountWin;
+        //public void ReviewGame()
+        //{
+        //    int stateCountWin = currentMoveHistory.Count;
+        //    int stateCountLose = stateCountWin;
 
-            double temp;
-            int[] coordinates;
+        //    double temp;
+        //    int[] coordinates;
 
-            if (currentMoveHistory.Count % 2 == 1)
-            {
-                stateCountWin--;
-            }
+        //    if (currentMoveHistory.Count % 2 == 1)
+        //    {
+        //        stateCountWin--;
+        //    }
 
-            temp = stateCountLose;
-            Console.WriteLine("Starting temp: {0}", temp);
-            for(int x = currentMoveHistory.Count - 1; x >= 0; x -= 2)
-            {
-                coordinates = currentMoveHistory[x];
-                states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / -stateCountLose);
-                Console.WriteLine("{0}, {1}, {2} has value of {3}", coordinates[0], coordinates[1], coordinates[2], states[coordinates[0], coordinates[1], coordinates[2]].averageState);       
-                temp--;
-            }
+        //    temp = stateCountLose;
+        //    Console.WriteLine("Starting temp: {0}", temp);
+        //    for(int x = currentMoveHistory.Count - 1; x >= 0; x -= 2)
+        //    {
+        //        coordinates = currentMoveHistory[x];
+        //        states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / -stateCountLose);
+        //        Console.WriteLine("{0}, {1}, {2} has value of {3}", coordinates[0], coordinates[1], coordinates[2], states[coordinates[0], coordinates[1], coordinates[2]].averageState);       
+        //        temp--;
+        //    }
 
-            temp = stateCountWin;
-            for (int x = currentMoveHistory.Count - 2; x >= 0; x -= 2)
-            {
-                coordinates = currentMoveHistory[x];
+        //    temp = stateCountWin;
+        //    for (int x = currentMoveHistory.Count - 2; x >= 0; x -= 2)
+        //    {
+        //        coordinates = currentMoveHistory[x];
 
-                states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / stateCountLose);
-                Console.WriteLine("{0}, {1}, {2} has value of {3}", coordinates[0], coordinates[1], coordinates[2], states[coordinates[0], coordinates[1], coordinates[2]].averageState);
+        //        states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / stateCountLose);
+        //        Console.WriteLine("{0}, {1}, {2} has value of {3}", coordinates[0], coordinates[1], coordinates[2], states[coordinates[0], coordinates[1], coordinates[2]].averageState);
                 
-                temp--;
-            }
-            DisplayAllStateValues();
-            CPUPlayer.RegisterNimStates(states);
+        //        temp--;
+        //    }
+        //    DisplayAllStateValues();
+        //    CPUPlayer.RegisterNimStates(states);
 
-            currentMoveHistory.Clear(); // This line should be the last one called in the method
-        }
-        public void DisplayAllStateValues()
-        {
-            for(int x = 0; x <= 3; x++)
-            {
-                for(int y = 0; y <= 5; y++)
-                {
-                    for(int z = 0; z <= 7; z++)
-                    {
-                        Console.WriteLine("{0}, {1}, {2} has a value of {3}", x,y,z,states[x,y,z].averageState);
-                    }
-                }
-            }
-        }
+        //    currentMoveHistory.Clear(); // This line should be the last one called in the method
+        //}
         public void DisplayBoard()
         {
             Console.Write("1:");
---
+++ GameReviewer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim
+{
+    class GameReviewer
+    {
+        List<int[]> currentMoveHistory = new List<int[]>();
+        NimState[,,] states = new NimState[4, 6, 8];
+        public GameReviewer(List<int[]> currentMoveHistory, NimState[,,] states)
+        {
+            this.currentMoveHistory = currentMoveHistory;
+            this.states = states;
+        }
+        public void ReviewGame()
+        {
+            int stateCountWin = currentMoveHistory.Count;
+            int stateCountLose = stateCountWin;
+
+            double temp;
+            int[] coordinates;
+
+            if (currentMoveHistory.Count % 2 == 1)
+            {
+                stateCountWin--;
+            }
+
+            temp = stateCountLose;
+            for (int x = currentMoveHistory.Count - 1; x >= 0; x -= 2)
+            {
+                coordinates = currentMoveHistory[x];
+                states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / -stateCountLose);
+                temp--;
+            }
+
+            temp = stateCountWin;
+            for (int x = currentMoveHistory.Count - 2; x >= 0; x -= 2)
+            {
+                coordinates = currentMoveHistory[x];
+                states[coordinates[0], coordinates[1], coordinates[2]].AppendAverage(temp / stateCountWin);
+                temp--;
+            }
+            temp = currentMoveHistory.Count;
+
+            CPUPlayer.RegisterNimStates(states);
+
+            currentMoveHistory.Clear(); // This line should be the last one called in the method
+        }
+    }
+}
