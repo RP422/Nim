825b2a8d352a440d5367f49193e137044dd21f77
Mike Lang <mlang@student.neumont.edu>
2016-10-03 11:52:38 -0600

    - Extracted GameController.cs - Removed unnecessary comments
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

--- Game.cs
+++ Game.cs
@@ -8,106 +8,20 @@ namespace Nim
 {
     public class Game
     {
-        private static NimState[,,] states = new NimState[4, 6, 8]; // The coordinates here correspond to the number of pieces in each row
-        private List<int[]> currentMoveHistory = new List<int[]>(); // The int arrays should always be 3 in length. They are corrdinates sets for states
         private bool turn;
-        
         Board board = new Board();
 
-        public static void Main(string[] args)
-        {
-            InitializeNimStates();
-            Game g = new Game();
-        }
-
-        private static void InitializeNimStates()
-        {
-            // Builds every NimState object for learning purposes throughout runtime
-            for (int x = 0; x < 4; x++)
-            {
-                for (int y = 0; y < 6; y++)
-                {
-                    for (int z = 0; z < 8; z++)
-                    {
-                        states[x, y, z] = new NimState(); // So, uh, funny story. This line of code once read [x, y, x] instead of [x, y, z]
-                    }
-                }
-            }
-        }
-
-        public Game()
+        public Game(NimState[,,] states)
         {
             CPUPlayer.RegisterNimStates(states);
-            StartGame();
         }
-        public void StartGame()
-        {
-            int quit;
-            do
-            {
-                quit = StartGameType(Menu());
-                board.ResetBoard();
-            } while (quit != 4);
-        }
-
-        //Gets user's input and returns an integer
-        public int Menu()
-        {
-            int x;
-            bool validInput;
-            do
-            {
-                Console.WriteLine("Pick an option\n1:Player vs Player\n2:Player vs CPU\n3:CPU vs CPU\n4:Quit");
-                validInput = int.TryParse(Console.ReadLine(), out x);
-            } while (!validInput || (x <= 0 || x > 4));
-            return x;
-        }
-        //Performs the action based on parameter passed in
-        public int StartGameType(int type)
-        {
-            DecideFirstMove();
-            switch (type)
-            {
-                //Start a PVP game
-                case 1:
-                    PlayGame(new HumanPlayer("1"), new HumanPlayer("2"));
-                    return type;
-                //Start a Player vs CPU game
-                case 2:
-                    PlayGame(new HumanPlayer("1"), new CPUPlayer());
-                    return type;
-                //Start a CPU vs CPU Game
-                //Ask how many games to play
-                case 3:
-                    int gameCount = GetNumCPUGames();
-                    for (int x = 0; x < gameCount; x++)
-                    {
-                        PlayGame(new CPUPlayer(), new CPUPlayer());
-                    }
-                    return type;
-                //Quit the game
-                default:
-                    return 4;
-            }
-        }
-        //Get the number of CPU Games to be played
-        //Returns an integer
-        public int GetNumCPUGames()
-        {
-            int numGames; bool valid;
-            do
-            {
-                Console.WriteLine("How many games would you like the CPU to play against itself");
-                valid = int.TryParse(Console.ReadLine(), out numGames);
-            } while (!valid || numGames <= 0);
-            return numGames;
-        }
-
+        
         public void DecideFirstMove()
         {
             Random r = new Random();
             turn = r.Next(2) == 0;
         }
+
         public void PlayGame(Player p1, Player p2)
         {                         
             bool done = false;
@@ -141,6 +55,12 @@ namespace Nim
         {
             return board.GameOver();
         }
+
+        public void Reset()
+        {
+            board.ResetBoard();
+        }
+
 
         //public void ReviewGame()
         //{
---
+++ GameController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim
+{
+    class GameController
+    {
+        private static NimState[,,] states = new NimState[4, 6, 8]; // The coordinates here correspond to the number of pieces in each row
+        private List<int[]> currentMoveHistory = new List<int[]>(); // The int arrays should always be 3 in length. They are corrdinates sets for states.
+
+        private Game g = new Game(states);
+
+        public static void Main(string[] args)
+        {
+            InitializeNimStates();
+            GameController control = new GameController();
+        }
+
+        public GameController()
+        {
+            StartGame();
+        }
+
+        private static void InitializeNimStates()
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    for (int z = 0; z < 8; z++)
+                    {
+                        states[x, y, z] = new NimState();
+                    }
+                }
+            }
+        }
+
+        public void StartGame()
+        {
+            int quit;
+            do
+            {
+                quit = StartGameType(Menu());
+                g.Reset();
+            } while (quit != 4);
+        }
+        public int StartGameType(int type)
+        {
+            g.DecideFirstMove();
+            switch (type)
+            {
+                //PVP game
+                case 1:
+                    g.PlayGame(new HumanPlayer("1"), new HumanPlayer("2"));
+                    return type;
+                //Player vs CPU game
+                case 2:
+                    g.PlayGame(new HumanPlayer("1"), new CPUPlayer());
+                    return type;
+                //CPU vs CPU Game(s)
+                case 3:
+                    int gameCount = GetNumCPUGames();
+                    for (int x = 0; x < gameCount; x++)
+                    {
+                        g.PlayGame(new CPUPlayer(), new CPUPlayer());
+                    }
+                    return type;
+                //Quit
+                default:
+                    return 4;
+            }
+        }
+        public int GetNumCPUGames()
+        {
+            int numGames; bool valid;
+            do
+            {
+                Console.WriteLine("How many games would you like the CPU to play against itself");
+                valid = int.TryParse(Console.ReadLine(), out numGames);
+            } while (!valid || numGames <= 0);
+            return numGames;
+        }
+
+        public int Menu()
+        {
+            int x;
+            bool validInput;
+            do
+            {
+                Console.WriteLine("Pick an option\n1:Player vs Player\n2:Player vs CPU\n3:CPU vs CPU\n4:Quit");
+                validInput = int.TryParse(Console.ReadLine(), out x);
+            } while (!validInput || (x <= 0 || x > 4));
+            return x;
+        }
+    }
+}
