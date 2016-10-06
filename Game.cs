6c0b79923ea1a54bf08c1aa7f5bba110bb15c9e2
mandalore <centerback_rules@yahoo.com>
2016-10-06 11:47:51 -0600

    creating type game classes

--- Game.cs
+++ Game.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace Nim
 {
-    public class Game
+    public abstract class Game
     {
         private static NimState[,,] states = new NimState[4, 6, 8];
-        
+        protected Player p1, p2;
         private bool turn;
         Board board = new Board();
 
         public Game(NimState[,,] states)
         {
             InitializeNimStates();
-            Game g = new Game();
             GameReviewer.RegisterNimStates(states);
         }
 
@@ -45,7 +44,7 @@ namespace Nim
             turn = r.Next(2) == 0;
         }
 
-        public void PlayGame(Player p1, Player p2)
+        public void PlayGame()
         {
             List<int[]> currentMoveHistory = new List<int[]>();
             bool done = false;
@@ -86,5 +85,7 @@ namespace Nim
         {
             return board;
         }
+        public abstract void CreatePlayers();
+        public abstract string GetPrompt();
     }
 }
